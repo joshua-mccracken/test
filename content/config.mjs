6e2e9164c38eb256829be637612db25d8d8a3f93
Joshua McCracken <62113878+joshua-mccracken@users.noreply.github.com>
2025-02-13 15:54:24 +1300

    updated website metadata

--- content/config.mjs
+++ content/config.mjs
@@ -5,8 +5,6 @@ const config = {
     author: "Joshua McCracken",
     logo: "",
     domain: "",
-    // links to the pages you want to link to in the navbar and in the footer
-    navLinks: [],
     navbarTitle: {
         text: "The Word of Adonai",
         logo: "", // optional
