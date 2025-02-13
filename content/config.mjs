7c20d78c4606b8e379f307125788b68d035a56f3
Joshua McCracken <62113878+joshua-mccracken@users.noreply.github.com>
2025-02-13 16:25:30 +1300

    updated config

--- content/config.mjs
+++ content/config.mjs
@@ -9,8 +9,19 @@ const config = {
         text: "The Word of Adonai",
         logo: "", // optional
       },
+    navLinks: [{ href: "/", name: "Home" }],
     search: {
         provider: "kbar",
+        kbarConfig: {
+            defaultActions: [
+              {
+                id: "blog",
+                name: "Blog",
+                keywords: "content",
+                perform: () => (window.location.pathname = "blog"),
+              },
+            ],
+          },
       },
     showToc: true,
   };
